e62d475e422b00729efed3a0e65b29f7715b575a
patel-poojan <poojanpatel5309@gmail.com>
2024-09-21 17:46:19 +0530

    fix: training and users page added

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -1,13 +1,129 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import DashboardLayout from "../components/DashboardLayout";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Input } from "@/components/ui/input";
+import { IoSearchSharp } from "react-icons/io5";
+import { FaQuoteLeft } from "react-icons/fa";
 
 const Page = () => {
+  const [tab, setTab] = useState(0);
+
   return (
     <DashboardLayout>
-      <div>training</div>
+      <div className="flex flex-1 overflow-hidden flex-col p-4 gap-4 sm:gap-6">
+        <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-3">
+          <p className="text-xl sm:text-2xl font-semibold text-black">
+            Training
+          </p>
+          <div className="flex items-center py-1 px-3 gap-2 rounded-xl bg-[#F8F8F8] w-full sm:w-auto">
+            <IoSearchSharp className="text-lg" />
+            <Input
+              className="w-full sm:w-32 border-none placeholder:text-[#1E255E] p-0 shadow-none focus-visible:ring-0"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <div
+            onClick={() => setTab(0)}
+            className={`cursor-pointer ${
+              tab === 0
+                ? "text-lg text-[#1E255E] font-medium underline underline-offset-8 decoration-2 decoration-[#57C0DD]"
+                : "text-base text-black font-light"
+            }`}
+          >
+            Unmatched Phrases
+          </div>
+          <div
+            onClick={() => setTab(1)}
+            className={`cursor-pointer ${
+              tab === 1
+                ? "text-lg text-[#1E255E] font-medium underline underline-offset-8 decoration-2 decoration-[#57C0DD]"
+                : "text-base text-black font-light"
+            }`}
+          >
+            Ignored
+          </div>
+        </div>
+
+        <div className="flex-1 flex flex-col overflow-auto">
+          {tab === 0 ? (
+            <TrainingTable type="unmatched" />
+          ) : (
+            <TrainingTable type="ignored" />
+          )}
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
 
 export default Page;
+
+export const TrainingTable = ({ type }: { type: string }) => {
+  const data = [
+    {
+      userQuery: "This is a dummy query, just for testing",
+      date: "Today 10:11 PM",
+    },
+    {
+      userQuery: "This is a dummy query, just for testing",
+      date: "Today 10:11 PM",
+    },
+  ];
+
+  return (
+    <Table className="min-w-full">
+      <TableHeader className="bg-[#57C0DD1A] backdrop-blur-3xl sticky top-0">
+        <TableRow>
+          <TableHead>
+            <div className="flex items-center justify-start gap-2">
+              User Query
+            </div>
+          </TableHead>
+          <TableHead>
+            <div className="flex items-center justify-center gap-2">Date</div>
+          </TableHead>
+          <TableHead>
+            <div className="flex items-center justify-center gap-2">Action</div>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((data, index) => (
+          <TableRow key={index} className="hover:bg-gray-50">
+            <TableCell>
+              <div className="flex items-start gap-2">
+                <FaQuoteLeft className="text-[#bbbbbb] text-lg sm:text-xl" />
+                <span className="break-all">{data.userQuery}</span>
+              </div>
+            </TableCell>
+            <TableCell className="text-center">{data.date}</TableCell>
+            <TableCell>
+              <div className="flex flex-wrap items-center justify-center gap-2">
+                <div className="py-1 px-5 rounded-md bg-[#57C0DD1A]">Train</div>
+                {type === "unmatched" && (
+                  <div className="py-1 px-5 rounded-md bg-[#F59B521A]">
+                    Ignore
+                  </div>
+                )}
+                <div className="py-1 px-5 rounded-md bg-[#FF02021A]">
+                  Delete
+                </div>
+              </div>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,14 +1,101 @@
 "use client";
 import React from "react";
 import DashboardLayout from "../components/DashboardLayout";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { FaRegCalendar, FaUser } from "react-icons/fa";
+import { MdAlternateEmail, MdSmartToy } from "react-icons/md";
+import { Input } from "@/components/ui/input";
+import { IoSearchSharp } from "react-icons/io5";
 
 const Page = () => {
+  const userDetails = [
+    {
+      name: "Prince",
+      email: "Prince@123",
+      lastTrainBot: "Today 10:11 PM",
+      totalBots: "04",
+    },
+    {
+      name: "Vandan",
+      email: "vandan@123",
+      lastTrainBot: "Today 10:11 PM",
+      totalBots: "04",
+    },
+    {
+      name: "Raj",
+      email: "raj@123",
+      lastTrainBot: "Today 10:11 PM",
+      totalBots: "04",
+    },
+  ];
   return (
-    <>
-      <DashboardLayout>
-        <div>users</div>
-      </DashboardLayout>
-    </>
+    <DashboardLayout>
+      <div className="flex flex-1 flex-col  overflow-hidden p-4 gap-4 sm:gap-6 max-w-full">
+        <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-3">
+          <p className="text-xl sm:text-2xl font-semibold text-black">User</p>
+          <div className="flex items-center py-1 px-3 gap-2 rounded-xl bg-[#F8F8F8] w-full sm:w-auto">
+            <IoSearchSharp className="text-lg" />
+            <Input
+              className="w-full sm:w-32 border-none placeholder:text-[#1E255E] p-0 shadow-none focus-visible:ring-0"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+        <div className="flex-1 flex flex-col overflow-auto">
+          <Table className="min-w-full">
+            <TableHeader className="bg-[#57C0DD1A] backdrop-blur-3xl sticky top-0">
+              <TableRow>
+                <TableHead className="py-2 text-start">
+                  <div className="flex items-center justify-start gap-1">
+                    <FaUser className="text-[#57C0DD] text=base hidden sm:block" />
+                    Name
+                  </div>
+                </TableHead>
+                <TableHead className="py-2 text-center">
+                  <div className="flex items-center justify-center gap-1">
+                    <MdAlternateEmail className="text-[#57C0DD] text=base hidden sm:block" />
+                    Email
+                  </div>
+                </TableHead>
+                <TableHead className="py-2 text-center">
+                  <div className="flex items-center justify-center gap-1">
+                    <FaRegCalendar className="text-[#57C0DD] text=base hidden sm:block" />
+                    Last Train Bot
+                  </div>
+                </TableHead>
+                <TableHead className="py-2 text-center">
+                  <div className="flex items-center justify-center gap-1">
+                    <MdSmartToy className="text-[#57C0DD] text=base hidden sm:block" />
+                    Total Bots
+                  </div>
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {userDetails.map((detail, index) => (
+                <TableRow key={index} className="hover:bg-gray-50">
+                  <TableCell className="text-left">{detail.name}</TableCell>
+                  <TableCell className="text-center">{detail.email}</TableCell>
+                  <TableCell className="text-center">
+                    {detail.lastTrainBot}
+                  </TableCell>
+                  <TableCell className="text-center">
+                    {detail.totalBots}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </DashboardLayout>
   );
 };
 
