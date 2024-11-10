11babd67ff43a64b98c4fd15baef43ab6e6c7396
vandan <vandanpatel29122001@gmail.com>
2024-11-11 00:35:08 +0530

    BDA modification done

--- src/app/(sidebar-group)/bda/page.tsx
+++ src/app/(sidebar-group)/bda/page.tsx
@@ -1,22 +1,18 @@
+"use client";
+import ChooseIndustryTemplate from "@/app/components/ChooseIndustryTemplate";
 import { Input } from "@/components/ui/input";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
 
 const BDA = () => {
-  // const {
-  //   // data: BDAQuestionList,
-  //   isLoading: loadBDAQuestionList,
-  //   isError: errorInBDAQuestionList,
-  // } = useQuery({
-  //   queryKey: ["BDAQuestion", "List"],
-  //   queryFn: fetchBDAQuestion,
-  //   enabled: industry && subIndustry ? true : false,
-  // });
+  const [industry, setIndustry] = useState<string>("");
+  const [subIndustry, setSubIndustry] = useState<string>("");
+
   return (
     <div className="flex flex-1 overflow-hidden flex-col max-[500px]:p-4 gap-4 sm:gap-6">
       <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-3">
-        <p className="text-xl sm:text-2xl font-semibold text-black">BDA</p>
+        <p className="text-xl sm:text-2xl font-semibold text-black">BDA Questions</p>
         <div className="flex items-center py-0 md:py-1 px-3 gap-2 rounded-xl bg-[#F8F8F8] w-full sm:w-auto">
           <IoSearchSharp className="text-lg" />
           <Input
@@ -26,7 +22,14 @@ const BDA = () => {
         </div>
       </div>
 
-      <div className="flex-1 flex flex-col overflow-auto"></div>
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChooseIndustryTemplate
+          up={() => {}}
+          setIndustry={setIndustry}
+          setSubIndustry={setSubIndustry}
+          adminAction={true}
+        />
+      </div>
     </div>
   );
 };
