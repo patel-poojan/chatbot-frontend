7f6a172b1e8423d93b3db034c505ff59e9cc6598
Vandan <vandan.p@honeysys.com>
2024-11-14 12:41:51 +0530

    Server file add

---
+++ server.js
@@ -0,0 +1,18 @@
+// app.js
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(10002, (err) => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:10002");
+  });
+});
