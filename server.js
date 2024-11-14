ab470c71d28d67bc4360e264af541ad1c2173ff3
Vandan <vandan.p@honeysys.com>
2024-11-14 12:44:02 +0530

    server updated

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// app.js
 import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
 
+const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -11,8 +11,7 @@ app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(10002, (err) => {
-    if (err) throw err;
-    console.log("> Ready on http://localhost:10002");
-  });
+  }).listen(port);
+
+  console.log(`> Server listening at http://localhost:${port} as ${dev ? "development" : process.env.NODE_ENV}`);
 });
