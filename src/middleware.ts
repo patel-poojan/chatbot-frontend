5943d7526a06dacb0695f6e41e2ca0d476722c90
vandan <vandanpatel29122001@gmail.com>
2025-02-24 21:45:56 +0530

    console.removed

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,8 +7,6 @@ export default function middleware(request: NextRequest) {
   // Allow access to specific routes without authentication
   const publicPaths = ["/document/privacy-policy.pdf", "/document/terms-of-use.pdf"];
 
-  console.log("pathName", pathName);
-
   if (publicPaths.includes(pathName)) {
     return NextResponse.next();
   }
