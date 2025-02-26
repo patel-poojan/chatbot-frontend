8d645e5a6e73cba00ad0c32beb3b613a8afcc33c
vandan <vandanpatel29122001@gmail.com>
2025-02-26 09:45:45 +0530

    toaster sec update

--- src/app/components/ClientProvider.tsx
+++ src/app/components/ClientProvider.tsx
@@ -1,18 +1,14 @@
-'use client';
+"use client";
 
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@/utils/react-query';
-import { Toaster } from 'sonner';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/utils/react-query";
+import { Toaster } from "sonner";
 
-export default function ClientProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function ClientProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <Toaster richColors position='top-right' duration={3000} />
+      <Toaster richColors position="top-right" duration={2000} />
     </QueryClientProvider>
   );
 }
