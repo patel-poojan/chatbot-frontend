4e9f7d00f61ab1fdfc3568410919d68f071bb7e7
vandan <vandanpatel29122001@gmail.com>
2025-05-12 23:00:13 +0530

    permission changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,24 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { useLogin } from '@/utils/auth-api';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import React, { useState, useCallback } from 'react';
-import { IoEyeOffOutline, IoEyeOutline } from 'react-icons/io5';
-import { toast } from 'sonner';
-import { Loader } from '../components/Loader';
-import { axiosError } from '../../types/axiosTypes';
-import Cookies from 'js-cookie';
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useLogin } from "@/utils/auth-api";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useState, useCallback } from "react";
+import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
+import { toast } from "sonner";
+import { Loader } from "../components/Loader";
+import { axiosError } from "../../types/axiosTypes";
+import Cookies from "js-cookie";
 
 const Page = () => {
   const router = useRouter();
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [passwordType, setPasswordType] = useState<string>('password');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordType, setPasswordType] = useState<string>("password");
 
   const togglePassword = useCallback(() => {
-    setPasswordType((prev) => (prev === 'password' ? 'text' : 'password'));
+    setPasswordType((prev) => (prev === "password" ? "text" : "password"));
   }, []);
 
   const { mutate: onLogin, isPending } = useLogin({
@@ -27,79 +27,68 @@ const Page = () => {
       const userRole = data.data.user.userRole;
       if (token) {
         if (data.data.user.username) {
-          Cookies.set('username', data.data.user.username, {
-            path: '/',
-            sameSite: 'Lax',
+          Cookies.set("username", data.data.user.username, {
+            path: "/",
+            sameSite: "Lax",
             secure: true,
           });
-          Cookies.set('email', data.data.user.email, {
-            path: '/',
-            sameSite: 'Lax',
+          Cookies.set("email", data.data.user.email, {
+            path: "/",
+            sameSite: "Lax",
             secure: true,
           });
         }
         if (data.data.user.userRole) {
-          Cookies.set('userRole', data.data.user.userRole, {
-            path: '/',
-            sameSite: 'Lax',
+          Cookies.set("userRole", data.data.user.userRole, {
+            path: "/",
+            sameSite: "Lax",
             secure: true,
           });
         }
+        console.log("data.data.user.permissions", data.data.user.permissions);
         if (data.data.user.permissions) {
-          Cookies.set(
-            'permissions',
-            JSON.stringify(data.data.user.permissions),
-            {
-              path: '/',
-              sameSite: 'Lax',
-              secure: true,
-            }
-          );
+          Cookies.set("permissions", JSON.stringify(data.data.user.permissions), {
+            path: "/",
+            sameSite: "Lax",
+            secure: true,
+          });
         }
-        Cookies.set('authToken', token, {
-          path: '/',
-          sameSite: 'Lax',
+        Cookies.set("authToken", token, {
+          path: "/",
+          sameSite: "Lax",
           secure: true,
         });
-        if (userRole === 'user') {
-          router.push('/statistics-dashboard-user');
-        } else if (userRole === 'admin') {
-          router.push('/statistics-dashboard');
-        } else if (userRole === 'subadmin') {
-          if (
-            data.data.user.permissions &&
-            data.data.user.permissions.includes('STATISTICAL_DASHBOARD')
-          ) {
-            router.push('/statistics-dashboard');
+        if (userRole === "user") {
+          router.push("/statistics-dashboard-user");
+        } else if (userRole === "admin") {
+          router.push("/statistics-dashboard");
+        } else if (userRole === "subadmin") {
+          if (data.data.user.permissions && data.data.user.permissions.includes("ADMIN_DASHBOARD")) {
+            router.push("/statistics-dashboard");
           } else {
-            router.push('/statistics-dashboard-user');
+            router.push("/statistics-dashboard-user");
           }
         } else {
-          router.push('/chatbotlist');
+          router.push("/chatbotlist");
         }
       }
       toast.success(data?.message);
     },
 
     onError(error: axiosError) {
-      const errorMessage =
-        error?.response?.data?.errors?.message ||
-        error?.response?.data?.message ||
-        'Login failed';
+      const errorMessage = error?.response?.data?.errors?.message || error?.response?.data?.message || "Login failed";
       toast.error(errorMessage);
     },
   });
 
   const handleSubmit = useCallback(
-    (
-      e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>
-    ) => {
+    (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) => {
       e.preventDefault();
 
       if (!email) {
-        toast.warning('Please fill in your email address');
+        toast.warning("Please fill in your email address");
       } else if (!password) {
-        toast.warning('Please fill in your password');
+        toast.warning("Please fill in your password");
       } else {
         onLogin({ email, password });
       }
@@ -108,80 +97,61 @@ const Page = () => {
   );
 
   return (
-    <div className='h-dvh w-dvw flex justify-center items-center'>
+    <div className="h-dvh w-dvw flex justify-center items-center">
       {isPending && <Loader />}
-      <form
-        onSubmit={handleSubmit}
-        className='flex flex-col gap-4 md:gap-6 w-full max-w-lg px-4'
-      >
-        <div className='flex flex-col gap-1 md:gap-2 justify-center items-center'>
-          <Image
-            src='/images/bot-icon.svg'
-            alt='chatbot logo'
-            width={45}
-            height={45}
-            priority
-            quality={100}
-          />
-          <p className='text-black font-medium text-2xl md:text-[32px]'>
-            Welcome back
-          </p>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 md:gap-6 w-full max-w-lg px-4">
+        <div className="flex flex-col gap-1 md:gap-2 justify-center items-center">
+          <Image src="/images/bot-icon.svg" alt="chatbot logo" width={45} height={45} priority quality={100} />
+          <p className="text-black font-medium text-2xl md:text-[32px]">Welcome back</p>
         </div>
-        <div className='w-full'>
-          <label htmlFor='email' className='text-black font-normal text-lg'>
+        <div className="w-full">
+          <label htmlFor="email" className="text-black font-normal text-lg">
             Email Address
           </label>
           <Input
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            id='email'
-            className='px-4 py-3 mt-1 rounded  focus-visible:ring-0 placeholder:text-sm placeholder:font-light w-full'
-            placeholder='Enter Your Email Address'
+            id="email"
+            className="px-4 py-3 mt-1 rounded  focus-visible:ring-0 placeholder:text-sm placeholder:font-light w-full"
+            placeholder="Enter Your Email Address"
           />
         </div>
-        <div className='w-full'>
-          <label htmlFor='password' className='text-black font-normal text-lg'>
+        <div className="w-full">
+          <label htmlFor="password" className="text-black font-normal text-lg">
             Password
           </label>
-          <div className='flex items-center border w-full rounded mt-1'>
+          <div className="flex items-center border w-full rounded mt-1">
             <Input
-              id='password'
+              id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               type={passwordType}
-              className='px-4 py-3 flex-1 !border-none rounded focus-visible:ring-0  placeholder:text-sm w-full'
-              placeholder='Enter Your Password'
+              className="px-4 py-3 flex-1 !border-none rounded focus-visible:ring-0  placeholder:text-sm w-full"
+              placeholder="Enter Your Password"
             />
-            <div className='cursor-pointer me-3' onClick={togglePassword}>
-              {passwordType === 'password' ? (
-                <IoEyeOffOutline />
-              ) : (
-                <IoEyeOutline />
-              )}
+            <div className="cursor-pointer me-3" onClick={togglePassword}>
+              {passwordType === "password" ? <IoEyeOffOutline /> : <IoEyeOutline />}
             </div>
           </div>
         </div>
-        <div className='flex justify-end font-normal text-sm sm:text-base '>
+        <div className="flex justify-end font-normal text-sm sm:text-base ">
           <a
-            href='#'
-            className='hover:underline underline-offset-1 text-[#57C0DD] hover:text-[#45A9B8]'
-            onClick={() => router.push('/forgetpassword')}
+            href="#"
+            className="hover:underline underline-offset-1 text-[#57C0DD] hover:text-[#45A9B8]"
+            onClick={() => router.push("/forgetpassword")}
           >
             Forgot Password?
           </a>
         </div>
         <Button
-          type='submit'
-          className='w-full text-white bg-gradient-to-r hover:from-[#53A7DD] hover:to-[#58C8DD]  from-[#58C8DD] to-[#53A7DD] py-3 rounded'
+          type="submit"
+          className="w-full text-white bg-gradient-to-r hover:from-[#53A7DD] hover:to-[#58C8DD]  from-[#58C8DD] to-[#53A7DD] py-3 rounded"
         >
           Log in
         </Button>
-        <div className='text-center text-[#1E255EB2] font-normal text-sm sm:text-lg'>
-          Don’t have an account?{' '}
-          <a
-            href='signup'
-            className='text-[#57C0DD] ms-1 hover:text-[#45A9B8] underline-offset-2 hover:underline'
-          >
+        <div className="text-center text-[#1E255EB2] font-normal text-sm sm:text-lg">
+          Don’t have an account?{" "}
+          <a href="signup" className="text-[#57C0DD] ms-1 hover:text-[#45A9B8] underline-offset-2 hover:underline">
             Sign up
           </a>
         </div>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import { jwtDecode } from 'jwt-decode';
-import { NextResponse, NextRequest } from 'next/server';
-import { toast } from 'sonner';
+import { jwtDecode } from "jwt-decode";
+import { NextResponse, NextRequest } from "next/server";
+import { toast } from "sonner";
 
 export default function middleware(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
 
   // Allow access to specific routes without authentication
-  const publicPaths = ['/privacy-policy.pdf', '/terms-of-use.pdf'];
+  const publicPaths = ["/privacy-policy.pdf", "/terms-of-use.pdf"];
 
   if (publicPaths.includes(pathName)) {
     return NextResponse.next();
@@ -14,17 +14,17 @@ export default function middleware(request: NextRequest) {
 
   try {
     // Retrieve the token from cookies
-    const token = request.cookies.get('authToken')?.value || '';
-    const role = request.cookies.get('userRole')?.value || '';
-    const permissions = request.cookies.get('permissions')?.value || '[]';
+    const token = request.cookies.get("authToken")?.value || "";
+    const role = request.cookies.get("userRole")?.value || "";
+    const permissions = request.cookies.get("permissions")?.value || "[]";
     const parsedPermissions = JSON.parse(permissions);
     // If no token is found, redirect to the home page
-    if (pathName === '/login' && !token) {
+    if (pathName === "/login" && !token) {
       return NextResponse.next();
     }
 
     if (!token) {
-      return NextResponse.redirect(new URL('/', request.url));
+      return NextResponse.redirect(new URL("/", request.url));
     }
 
     // Decode the token and check its expiration
@@ -33,40 +33,33 @@ export default function middleware(request: NextRequest) {
     // Check role-based access control
     // Check role-based access control
     if (
-      pathName === '/users' &&
-      (role === 'user' ||
-        (role === 'subadmin' &&
-          !parsedPermissions.includes('MANAGE_USERS') &&
-          !parsedPermissions.includes('ADMIN_DASHBOARD')))
+      pathName === "/users" &&
+      (role === "user" || (role === "subadmin" && !parsedPermissions.includes("MANAGE_USERS")))
     ) {
-      toast.error('You do not have permission to access this resource');
-      return NextResponse.redirect(new URL('/chatbotlist', request.url));
+      toast.error("You do not have permission to access this resource");
+      return NextResponse.redirect(new URL("/chatbotlist", request.url));
     }
 
     // Check role-based access control for bda route
     if (
-      pathName === '/bda' &&
-      (role === 'user' ||
-        (role === 'subadmin' &&
-          !parsedPermissions.includes('BDA_QUESTION_MANAGEMENT')))
+      pathName === "/bda" &&
+      (role === "user" || (role === "subadmin" && !parsedPermissions.includes("BDA_QUESTION_MANAGEMENT")))
     ) {
-      toast.error('You do not have permission to access this resource');
-      return NextResponse.redirect(new URL('/chatbotlist', request.url));
+      toast.error("You do not have permission to access this resource");
+      return NextResponse.redirect(new URL("/chatbotlist", request.url));
     }
 
     if (
-      pathName === '/statistics-dashboard' &&
-      (role === 'user' ||
-        (role === 'subadmin' &&
-          !parsedPermissions.includes('STATISTICAL_DASHBOARD')))
+      pathName === "/statistics-dashboard" &&
+      (role === "user" || (role === "subadmin" && !parsedPermissions.includes("ADMIN_DASHBOARD")))
     ) {
-      toast.error('You do not have permission to access this resource');
-      return NextResponse.redirect(new URL('/chatbotlist', request.url));
+      toast.error("You do not have permission to access this resource");
+      return NextResponse.redirect(new URL("/chatbotlist", request.url));
     }
 
-    if (pathName === '/login' && token) {
+    if (pathName === "/login" && token) {
       if (decoded.exp * 1000 > new Date().getTime()) {
-        return NextResponse.redirect(new URL('/chatbotlist', request.url));
+        return NextResponse.redirect(new URL("/chatbotlist", request.url));
       } else {
         return NextResponse.next();
       }
@@ -76,37 +69,37 @@ export default function middleware(request: NextRequest) {
         return NextResponse.next();
       } else {
         // If the token is expired, redirect to the home page
-        return NextResponse.redirect(new URL('/', request.url));
+        return NextResponse.redirect(new URL("/", request.url));
       }
     }
   } catch (error) {
     // If there's an error (e.g., invalid token), redirect to the home page
-    if (pathName === '/login') {
+    if (pathName === "/login") {
       return NextResponse.next();
     }
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 }
 
 export const config = {
   matcher: [
-    '/chatbotlist',
-    '/create',
-    '/create/document',
-    '/create/document/train',
-    '/create/website',
-    '/create/website/train',
-    '/users',
-    '/training',
-    '/login',
-    '/create/website/:path*',
-    '/create/document/:path*',
-    '/create/website/train/:path*',
-    '/create/document/train/:path*',
-    '/dashboard',
-    '/dashboard/:path*',
-    '/bda',
-    '/statistics-dashboard',
-    '/statistics-dashboard-user',
+    "/chatbotlist",
+    "/create",
+    "/create/document",
+    "/create/document/train",
+    "/create/website",
+    "/create/website/train",
+    "/users",
+    "/training",
+    "/login",
+    "/create/website/:path*",
+    "/create/document/:path*",
+    "/create/website/train/:path*",
+    "/create/document/train/:path*",
+    "/dashboard",
+    "/dashboard/:path*",
+    "/bda",
+    "/statistics-dashboard",
+    "/statistics-dashboard-user",
   ],
 };
