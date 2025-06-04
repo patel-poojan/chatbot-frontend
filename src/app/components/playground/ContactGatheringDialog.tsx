b5cfe93274de17371b4acff76224e661986e5eaf
patel-poojan <poojanpatel5309@gmail.com>
2025-06-05 00:09:05 +0530

    fix: change text of download button

--- src/app/components/playground/ContactGatheringDialog.tsx
+++ src/app/components/playground/ContactGatheringDialog.tsx
@@ -278,7 +278,7 @@ const ContactGatheringDialog = ({
                 d='M12 10v6m0 0l-3-3m3 3l3-3M3 17V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2z'
               />
             </svg>
-            Download Contact Details
+            Download Submitted Contacts
           </Button>
           <div className='flex flex-col-reverse sm:flex-row sm:justify-end gap-3'>
             <Button
