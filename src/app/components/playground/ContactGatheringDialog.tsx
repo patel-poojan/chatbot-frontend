f34e15ef460c19076756763b7de9f89b4557c79c
patel-poojan <poojanpatel5309@gmail.com>
2025-06-05 12:47:34 +0530

    fix: change downloaded file name

--- src/app/components/playground/ContactGatheringDialog.tsx
+++ src/app/components/playground/ContactGatheringDialog.tsx
@@ -116,7 +116,7 @@ const ContactGatheringDialog = ({
           url = window.URL.createObjectURL(data);
           const a = document.createElement('a');
           a.href = url;
-          a.download = `contacts_${chatbotId}_${
+          a.download = `Submitted_Contacts_${chatbotId}_${
             new Date().toISOString().split('T')[0]
           }.csv`;
 
